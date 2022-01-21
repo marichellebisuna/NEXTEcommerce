ea407ffa9b22493f0e389bbe5da882185e292306
Marichelle Bisuna <marichellebisuna@yahoo.com>
2022-01-21 16:50:29 +1100

    Products page with getServerSideProps done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,41 @@
-const Home=()=>{
+import Head from 'next/head'
+import { useState} from 'react'
+import { getData } from '../utils/fetchData'
+import ProductItem from '../components/product/ProductItem'
+
+const Home = (props) => {
+  const [products, setProducts] = useState(props.products)
+
   return(
-   <div>Home</div>
+    <div className="home_page">
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
+      <div className="products">
+        {
+          products.length === 0 
+          ? <h2>No Products</h2>
+
+          : products.map(product => (
+            <ProductItem key={product._id} product={product}  />
+          ))
+        }
+      </div>
+    </div>
   )
- }
- export default Home
+}
+
+
+export async function getServerSideProps() {
+ const res = await getData('product')
+
+ console.log(res)
+  return {
+    props: {
+      products: res.products,      
+    }, 
+  }
+}
+
+export default Home
