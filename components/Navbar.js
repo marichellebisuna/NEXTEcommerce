c3ff9b66d9feca41afa0f76559505145060a2503
Marichelle Bisuna <marichellebisuna@yahoo.com>
2022-01-21 21:35:39 +1100

    Product Details Page done

--- components/Navbar.js
+++ components/Navbar.js
@@ -51,7 +51,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <Link  href="/">
-        <a className="navbar-brand">DEVAT</a>
+        <a className="navbar-brand">MICH/Mart</a>
     </Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
